56c92129ae3aeec1b03904c794a065d99b5c3d5d
mdacht777 <mdacht777@gmail.com>
2020-10-25 08:31:14 -0600

    commit tree c88e032866d2e31b1c33a40c430e465597f4ffd4

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+fetch(imgUrl)
+.then(function(response) {
+  return response.json();
+}).then(function(json) {
+  console.log(json);
+});
+
+for (const char of response.json) {
+  console.log(char);
+}
